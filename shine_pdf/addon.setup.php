7aaeb41e91a64ef22451a689ff3af48b088e8606
Thomas <thomas@knaller.info>
2021-05-28 11:07:27 +0200

    add ee>=3 addon info; upgrade mpdf to current

---
+++ shine_pdf/addon.setup.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+return array(
+    'author' => 'Shine Marketing',
+    'author_url' => 'https://devot-ee.com/add-ons/shine-pdf',
+    'description' => 'Generates PDFs on-the-fly from ExpressionEngine templates',
+    'docs_url' => 'https://devot-ee.com/add-ons/shine-pdf',
+    'name' => 'Shine PDF',
+    'settings_exist' => false,
+    'version' => '0.1',
+    'namespace'   => 'shine_pdf/Shine_pdf'
+); 
